d8f688f5f11f0d7b447fc1af578d78353b753270
Xavier <xl.perrin@gmail.com>
2019-05-15 01:54:26 +0200

    implement PatientDAO and fix typo Patient

--- TPHopital/Classes/DAO/PatientDAO.cs
+++ TPHopital/Classes/DAO/PatientDAO.cs
@@ -15,15 +15,31 @@ namespace TPHopital.Classes.DAO
         public PatientDAO()
         {
             connection = Connection.Instance;
-            createCmd = new SqlCommand("INSERT INTO Client (nom,prenom) values(@nom,@prenom)", connection);
-            retrieveCmd = new SqlCommand("SELECT * FROM Client where nom like @search OR prenom like @search", connection);
-            updateCmd = new SqlCommand("UPDATE client SET nom='@nom', prenom='@prenom', tel='@tel' WHERE id=@id");
-            deleteCmd = new SqlCommand("DELETE FROM client WHERE id=@id ");
+            createCmd = new SqlCommand("INSERT INTO Patient (nom, prenom, date_naissance, sexe, adresse, situation_familiale) values(@nom, @prenom, @date_naissance, @sexe, @adresse, @situation_familiale)", connection);
+            retrieveCmd = new SqlCommand("SELECT * FROM Patient where nom like @search OR prenom like @search", connection);
+            updateCmd = new SqlCommand("UPDATE Patient SET nom='@nom', prenom='@prenom', tel='@tel' WHERE id=@id");
+            deleteCmd = new SqlCommand("DELETE FROM Patient WHERE id=@id ");
         }
 
         public void Create(Patient patient)
         {
-            throw new NotImplementedException();
+            createCmd.Parameters.Add(new SqlParameter("@nom", patient.NomPatient));
+            createCmd.Parameters.Add(new SqlParameter("@prenom", patient.PrenomPatient));
+            createCmd.Parameters.Add(new SqlParameter("@date_naissance", patient.DateNaissance));
+            createCmd.Parameters.Add(new SqlParameter("@sexe", patient.Sexe));
+            /**
+             etc
+             **/
+
+            connection.Open();
+
+            if (createCmd.ExecuteNonQuery() > 0)
+            {
+                Console.WriteLine("Insertion effecutée");
+            }
+
+            createCmd.Dispose();
+            connection.Close();
         }
 
         public void Delete(int id)
--- TPHopital/Classes/Patient.cs
+++ TPHopital/Classes/Patient.cs
@@ -11,12 +11,12 @@ namespace TPHopital.Classes
         private string nomPatient;
         private string prenomPatient;
         private DateTime dateNaissance;
-        private string Sexe;
-        private string Adresse;
+        private string sexe;
+        private string adresse;
         private string situationFamiliale;
         private string assuranceMedicale;
         private string codeassurance;
-        private int Tel;
+        private int tel;
         private string nomPere;
         private string nomMere;
         private string nom_P_a_prevenir;
@@ -26,12 +26,12 @@ namespace TPHopital.Classes
         public string NomPatient { get => nomPatient; set => nomPatient = value; }
         public string PrenomPatient { get => prenomPatient; set => prenomPatient = value; }
         public DateTime DateNaissance { get => dateNaissance; set => dateNaissance = value; }
-        public string Sexe1 { get => Sexe; set => Sexe = value; }
-        public string Adresse1 { get => Adresse; set => Adresse = value; }
+        public string Sexe { get => Sexe; set => Sexe = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
         public string SituationFamiliale { get => situationFamiliale; set => situationFamiliale = value; }
         public string AssuranceMedicale { get => assuranceMedicale; set => assuranceMedicale = value; }
         public string Codeassurance { get => codeassurance; set => codeassurance = value; }
-        public int Tel1 { get => Tel; set => Tel = value; }
+        public int Tel { get => tel; set => tel = value; }
         public string NomPere { get => nomPere; set => nomPere = value; }
         public string NomMere { get => nomMere; set => nomMere = value; }
         public string Nom_P_a_prevenir { get => nom_P_a_prevenir; set => nom_P_a_prevenir = value; }
