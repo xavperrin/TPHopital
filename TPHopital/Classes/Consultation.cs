67f6d5f6186cbf0e570f964132b8c786ecf8262e
Xavier Perrin <xl.perrin@gmail.com>
2019-05-15 13:46:10 +0300

    Consultation : fk_id_type_consultation_id

--- TPHopital/Classes/Consultation.cs
+++ TPHopital/Classes/Consultation.cs
@@ -9,11 +9,11 @@ namespace TPHopital.Classes
         private int id_consultation;
         private DateTime date_consultation;
         private string synthese;
-        private int fk_id_type_consultation;
+        private int type_consultation_id;
 
         public int Id_consultation { get => id_consultation; set => id_consultation = value; }
         public DateTime Date_consultation { get => date_consultation; set => date_consultation = value; }
         public string Synthese { get => synthese; set => synthese = value; }
-        public int Fk_id_type_consultation { get => fk_id_type_consultation; set => fk_id_type_consultation = value; }
+        public int Fk_id_type_consultation { get => type_consultation_id; set => type_consultation_id = value; }
     }
 }
