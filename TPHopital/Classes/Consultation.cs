78e609a026614da4cdde0b0c13e42ddf4603abc7
Xavier Perrin <xl.perrin@gmail.com>
2019-05-15 14:26:44 +0300

    implementation de ConsultationDAO & fix Consultation

--- TPHopital/Classes/Consultation.cs
+++ TPHopital/Classes/Consultation.cs
@@ -14,6 +14,6 @@ namespace TPHopital.Classes
         public int Id_consultation { get => id_consultation; set => id_consultation = value; }
         public DateTime Date_consultation { get => date_consultation; set => date_consultation = value; }
         public string Synthese { get => synthese; set => synthese = value; }
-        public int Fk_id_type_consultation { get => type_consultation_id; set => type_consultation_id = value; }
+        public int Type_consultation_id { get => type_consultation_id; set => type_consultation_id = value; }
     }
 }
