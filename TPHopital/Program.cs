8afd829e13e97f69169157ec37acbd4e6d7396ff
Xavier Perrin <xl.perrin@gmail.com>
2019-05-17 14:11:16 +0300

    implementation NewPatient()

--- TPHopital/Program.cs
+++ TPHopital/Program.cs
@@ -149,11 +149,44 @@ namespace TPHopital
         static void NewPatient()
         {
             Patient p = new Patient();
-                Console.WriteLine("Entrez nom");
+            Console.WriteLine("Entrez nom");
             p.NomPatient = Console.ReadLine();
             Console.WriteLine("Entrez prénom");
             p.PrenomPatient = Console.ReadLine();
-           
+            Console.WriteLine("Entrez jour de naissance");
+            int j=Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez mois de naissance");
+            int m= Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez année de naissance");
+            int y = Int32.Parse(Console.ReadLine());
+
+            p.DateNaissance = new DateTime(y,m,j);
+
+
+            Console.WriteLine("Entrez le genre du patient\n 1 : Homme\n 2 : Femme)");
+            int intSexe = Int32.Parse(Console.ReadLine());
+            if (intSexe == 1)
+            {
+                p.Sexe = "Homme";
+            }
+            else p.Sexe = "Femme";
+
+            Console.WriteLine("adresse du patient");
+            p.Adresse = Console.ReadLine();
+
+            Console.WriteLine("Entrez la situation familiale\n 1 : célibataire\n 2 : marié(e))");
+            int intSitFamille = Int32.Parse(Console.ReadLine());
+            if (intSitFamille == 1)
+            {
+                p.SituationFamiliale = "celibataire";
+            }
+            else p.SituationFamiliale = "en couple";
+
+            Console.WriteLine("Entrez l'assurance médicale :");
+            p.AssuranceMedicale = Console.ReadLine();
+            Console.WriteLine("Entrez le numero de téléphone :");
+            p.Tel= Console.ReadLine();
+
             FacadeMetier.Add(p);
 
         }
