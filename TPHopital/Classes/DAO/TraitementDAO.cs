964c73ee46f06453ff35e8af45c72dcedde5381e
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-05-17 12:54:16 +0200

    Modification DAOs Traitement

--- TPHopital/Classes/DAO/TraitementDAO.cs
+++ TPHopital/Classes/DAO/TraitementDAO.cs
@@ -11,19 +11,12 @@ namespace TPHopital.Classes.DAO
         protected SqlCommand updateCmd;
         protected SqlCommand deleteCmd;
         protected SqlConnection connection;
-        protected SqlCommand listAllCmd;
-        
-
-
+        protected SqlCommand listAllCmd;   
 
         public TraitementDAO()
         {
             connection = Connection.Instance;
-            //createCmd = new SqlCommand("INSERT INTO Traitement (nom_medecin, prenom_medecin, tel_medecin) values(@nom, @prenom, @tel)", connection);
-            //retrieveCmd = new SqlCommand("SELECT * FROM Traiement where id_traitement like @search", connection);
-            //updateCmd = new SqlCommand("UPDATE Medecin SET nom_medecin='@nom', prenom_medecin='@prenom', tel_medecin='@tel' WHERE id=@id", connection);
             deleteCmd = new SqlCommand("DELETE FROM traitement WHERE id_traitement=@id ", connection);
-            //retrieveAll = new SqlCommand("SELECT * FROM Traitement");
         }
 
         public  void Create(Traitement t)
