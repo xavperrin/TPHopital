b0fbaf8c5e19db8d61c706e4d299007ccc38fbc4
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-05-17 16:29:47 +0200

    AjoutThread Patient

--- TPHopital/Classes/DAO/PatientDAO.cs
+++ TPHopital/Classes/DAO/PatientDAO.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-
+using System.Threading.Tasks;
 
 namespace TPHopital.Classes.DAO
 {
@@ -26,144 +26,175 @@ namespace TPHopital.Classes.DAO
 
         public void Create(Patient patient)
         {
-            string format = "yyyy-MM-dd HH:mm:ss";
-
-            createCmd.Parameters.Add(new SqlParameter("@nom", patient.NomPatient));
-            createCmd.Parameters.Add(new SqlParameter("@prenom", patient.PrenomPatient));
-            createCmd.Parameters.Add(new SqlParameter("@date", patient.DateNaissance));
-            createCmd.Parameters.Add(new SqlParameter("@sexe", patient.Sexe));
-            createCmd.Parameters.Add(new SqlParameter("@adresse", patient.Adresse));
-            createCmd.Parameters.Add(new SqlParameter("@situation", patient.SituationFamiliale));
-            createCmd.Parameters.Add(new SqlParameter("@assurance", patient.AssuranceMedicale));
-            createCmd.Parameters.Add(new SqlParameter("@codeA", patient.Codeassurance));
-            createCmd.Parameters.Add(new SqlParameter("@tel", patient.Tel));
-            createCmd.Parameters.Add(new SqlParameter("@nomP", patient.NomPere));
-            createCmd.Parameters.Add(new SqlParameter("@nomM", patient.NomMere));
-            createCmd.Parameters.Add(new SqlParameter("@nomPaP", patient.Nom_P_a_prevenir));
-            createCmd.Parameters.Add(new SqlParameter("@telPaP", patient.Tel_P_a_prevenir));
-
-            connection.Open();
-            Console.WriteLine(patient);
-            //Console.ReadLine();
-            if (createCmd.ExecuteNonQuery() > 0)
+            Task t = Task.Run(() =>
             {
-                Console.WriteLine("Insertion effecutée" + patient);
-            }
-            else throw new InsertPatientException("Insertion n'a pas réussie"+patient);
+                string format = "yyyy-MM-dd HH:mm:ss";
+                createCmd.Parameters.Clear();
 
-            createCmd.Dispose();
-            connection.Close();
+                createCmd.Parameters.Add(new SqlParameter("@nom", patient.NomPatient));
+                createCmd.Parameters.Add(new SqlParameter("@prenom", patient.PrenomPatient));
+                createCmd.Parameters.Add(new SqlParameter("@date", patient.DateNaissance));
+                createCmd.Parameters.Add(new SqlParameter("@sexe", patient.Sexe));
+                createCmd.Parameters.Add(new SqlParameter("@adresse", patient.Adresse));
+                createCmd.Parameters.Add(new SqlParameter("@situation", patient.SituationFamiliale));
+                createCmd.Parameters.Add(new SqlParameter("@assurance", patient.AssuranceMedicale));
+                createCmd.Parameters.Add(new SqlParameter("@codeA", patient.Codeassurance));
+                createCmd.Parameters.Add(new SqlParameter("@tel", patient.Tel));
+                createCmd.Parameters.Add(new SqlParameter("@nomP", patient.NomPere));
+                createCmd.Parameters.Add(new SqlParameter("@nomM", patient.NomMere));
+                createCmd.Parameters.Add(new SqlParameter("@nomPaP", patient.Nom_P_a_prevenir));
+                createCmd.Parameters.Add(new SqlParameter("@telPaP", patient.Tel_P_a_prevenir));
+
+                lock (new object())
+                {
+                    connection.Open();
+                    Console.WriteLine(patient);
+                    //Console.ReadLine();
+                    if (createCmd.ExecuteNonQuery() > 0)
+                    {
+                        Console.WriteLine("Insertion effecutée" + patient);
+                    }
+                    else throw new InsertPatientException("Insertion n'a pas réussie" + patient);
+
+                    createCmd.Dispose();
+                    connection.Close();
+                }
+            });
+            t.Wait();
         }
 
         public void Delete(int id)
         {
-            deleteCmd.Parameters.Add(new SqlParameter("@id", id));
-
-            connection.Open();
-
-            if (deleteCmd.ExecuteNonQuery() > 0)
+            Task.Run(() =>
             {
-                Console.WriteLine("Suppression effecutée");
-            }
+                deleteCmd.Parameters.Add(new SqlParameter("@id", id));
+                lock (new object())
+                {
+                    connection.Open();
 
-            deleteCmd.Dispose();
-            connection.Close();
+                    if (deleteCmd.ExecuteNonQuery() > 0)
+                    {
+                        Console.WriteLine("Suppression effecutée");
+                    }
+
+                    deleteCmd.Dispose();
+                    connection.Close();
+                }
+            });
         }
 
         public Patient Retrieve(int id)
         {
-            Patient patient = new Patient();
-
-            retrieveCmd.Parameters.Add(new SqlParameter("@search", id));
-
-            connection.Open();
-
-
-            SqlDataReader reader = retrieveCmd.ExecuteReader();
-
-            if (reader.Read())
+            Task<Patient> t = Task.Run(() =>
             {
-                patient.Id_Patient = reader.GetInt32(0);
-                patient.NomPatient = reader.GetString(1);
-                patient.PrenomPatient = reader.GetString(2);
-                patient.DateNaissance = reader.GetDateTime(3);
-                patient.Sexe = reader.GetString(4);
-                patient.Adresse = reader.GetString(5);
-                patient.SituationFamiliale = reader.GetString(6);
-                patient.AssuranceMedicale = reader.GetString(7);
-                patient.Codeassurance = reader.GetString(8);
-                patient.Tel = reader.GetString(9);
-                patient.NomPere = reader.GetString(10);
-                patient.NomMere = reader.GetString(11);
-                patient.Nom_P_a_prevenir = reader.GetString(12);
-                patient.Tel_P_a_prevenir = reader.GetString(13);
-            }
+                Patient patient = new Patient();
 
-            reader.Close();
-            retrieveCmd.Dispose();
-            connection.Close();
+                retrieveCmd.Parameters.Add(new SqlParameter("@search", id));
 
-            return patient;
+                lock (new object())
+                {
+                    connection.Open();
+                    SqlDataReader reader = retrieveCmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        patient.Id_Patient = reader.GetInt32(0);
+                        patient.NomPatient = reader.GetString(1);
+                        patient.PrenomPatient = reader.GetString(2);
+                        patient.DateNaissance = reader.GetDateTime(3);
+                        patient.Sexe = reader.GetString(4);
+                        patient.Adresse = reader.GetString(5);
+                        patient.SituationFamiliale = reader.GetString(6);
+                        patient.AssuranceMedicale = reader.GetString(7);
+                        patient.Codeassurance = reader.GetString(8);
+                        patient.Tel = reader.GetString(9);
+                        patient.NomPere = reader.GetString(10);
+                        patient.NomMere = reader.GetString(11);
+                        patient.Nom_P_a_prevenir = reader.GetString(12);
+                        patient.Tel_P_a_prevenir = reader.GetString(13);
+                    }
+
+                    reader.Close();
+                    retrieveCmd.Dispose();
+                    connection.Close();
+
+                    return patient;
+                }
+            });
+
+            return t.Result;
         }
 
         public void Update(Patient patient, int id)
         {
-            updateCmd.Parameters.Add(new SqlParameter("@nom", patient.NomPatient));
-            updateCmd.Parameters.Add(new SqlParameter("@prenom", patient.PrenomPatient));
-            updateCmd.Parameters.Add(new SqlParameter("@date", patient.DateNaissance));
-            updateCmd.Parameters.Add(new SqlParameter("@sexe", patient.Sexe));
-            updateCmd.Parameters.Add(new SqlParameter("@adresse", patient.Adresse));
-            updateCmd.Parameters.Add(new SqlParameter("@situation", patient.SituationFamiliale));
-            updateCmd.Parameters.Add(new SqlParameter("@assurance", patient.AssuranceMedicale));
-            updateCmd.Parameters.Add(new SqlParameter("@codeA", patient.Codeassurance));
-            updateCmd.Parameters.Add(new SqlParameter("@tel", patient.Tel));
-            updateCmd.Parameters.Add(new SqlParameter("@nomP", patient.NomPere));
-            updateCmd.Parameters.Add(new SqlParameter("@nomM", patient.NomMere));
-            updateCmd.Parameters.Add(new SqlParameter("@nomPaP", patient.Nom_P_a_prevenir));
-            updateCmd.Parameters.Add(new SqlParameter("@telPaP", patient.Tel_P_a_prevenir));
+            Task t = Task.Run(() =>
+            {
+                updateCmd.Parameters.Add(new SqlParameter("@nom", patient.NomPatient));
+                updateCmd.Parameters.Add(new SqlParameter("@prenom", patient.PrenomPatient));
+                updateCmd.Parameters.Add(new SqlParameter("@date", patient.DateNaissance));
+                updateCmd.Parameters.Add(new SqlParameter("@sexe", patient.Sexe));
+                updateCmd.Parameters.Add(new SqlParameter("@adresse", patient.Adresse));
+                updateCmd.Parameters.Add(new SqlParameter("@situation", patient.SituationFamiliale));
+                updateCmd.Parameters.Add(new SqlParameter("@assurance", patient.AssuranceMedicale));
+                updateCmd.Parameters.Add(new SqlParameter("@codeA", patient.Codeassurance));
+                updateCmd.Parameters.Add(new SqlParameter("@tel", patient.Tel));
+                updateCmd.Parameters.Add(new SqlParameter("@nomP", patient.NomPere));
+                updateCmd.Parameters.Add(new SqlParameter("@nomM", patient.NomMere));
+                updateCmd.Parameters.Add(new SqlParameter("@nomPaP", patient.Nom_P_a_prevenir));
+                updateCmd.Parameters.Add(new SqlParameter("@telPaP", patient.Tel_P_a_prevenir));
 
-            updateCmd.Parameters.Add(new SqlParameter("@id", id));
+                lock (new object())
+                {
+                    updateCmd.Parameters.Add(new SqlParameter("@id", id));
 
-            connection.Open();
-            updateCmd.ExecuteNonQuery();
-            updateCmd.Dispose();
-            connection.Close();
+                    connection.Open();
+                    updateCmd.ExecuteNonQuery();
+                    updateCmd.Dispose();
+                    connection.Close();
+                }
+            });
         }
 
         public List<Patient> ListAll()
         {
-            List<Patient> listPatient = new List<Patient>();
-
-            connection.Open();
-
-            SqlDataReader reader = listAllCmd.ExecuteReader();
-
-            while (reader.Read())
+            Task<List<Patient>> t = Task.Run(new Func<List<Patient>>(() =>
             {
-                listPatient.Add(new Patient
+                List<Patient> listPatient = new List<Patient>();
+
+                lock (new object())
                 {
-                    Id_Patient = reader.GetInt32(0),
-                    NomPatient = reader.GetString(1),
-                    PrenomPatient = reader.GetString(2),
-                    DateNaissance = reader.GetDateTime(3),
-                    Sexe = reader.GetString(4),
-                    Adresse = reader.GetString(5),
-                    SituationFamiliale = reader.GetString(6),
-                    AssuranceMedicale = reader.GetString(7),
-                    Codeassurance = reader.GetString(8),
-                    Tel = reader.GetString(9),
-                    NomPere = reader.GetString(10),
-                    NomMere = reader.GetString(11),
-                    Nom_P_a_prevenir = reader.GetString(12),
-                    Tel_P_a_prevenir = reader.GetString(13)
-                });
-            }
+                    connection.Open();
+                    SqlDataReader reader = listAllCmd.ExecuteReader();
 
-            reader.Close();
-            retrieveCmd.Dispose();
-            connection.Close();
+                    while (reader.Read())
+                    {
+                        listPatient.Add(new Patient
+                        {
+                            Id_Patient = reader.GetInt32(0),
+                            NomPatient = reader.GetString(1),
+                            PrenomPatient = reader.GetString(2),
+                            DateNaissance = reader.GetDateTime(3),
+                            Sexe = reader.GetString(4),
+                            Adresse = reader.GetString(5),
+                            SituationFamiliale = reader.GetString(6),
+                            AssuranceMedicale = reader.GetString(7),
+                            Codeassurance = reader.GetString(8),
+                            Tel = reader.GetString(9),
+                            NomPere = reader.GetString(10),
+                            NomMere = reader.GetString(11),
+                            Nom_P_a_prevenir = reader.GetString(12),
+                            Tel_P_a_prevenir = reader.GetString(13)
+                        });
+                    }
 
-            return listPatient;
+                    reader.Close();
+                    retrieveCmd.Dispose();
+                    connection.Close();
+
+                    return listPatient;
+                }
+            }));
+            return t.Result;
         }
     }
 }
